f98bfdfca742aa4df21d86f2ef7f10bc5aeec29b
EvangelosTsakanikas <evangelostsakanikas@gmail.com>
2024-11-29 22:04:05 +0100

    Initial Commit

---
+++ TodoList/TodoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoListApp.swift
+//  TodoList
+//
+//  Created by user268168 on 11/29/24.
+//
+
+import SwiftUI
+
+@main
+struct TodoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
