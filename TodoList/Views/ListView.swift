07d94a4b3994b045615669da248b397e89490c49
EvangelosTsakanikas <evangelostsakanikas@gmail.com>
2024-11-29 22:35:54 +0100

    Created Item Model and used it on the list rows (List Row). Also did some minor stylistic fixes.

--- TodoList/Views/ListView.swift
+++ TodoList/Views/ListView.swift
@@ -9,17 +9,15 @@ import SwiftUI
 
 struct ListView: View {
     
-    @State var items: [String] = [
-        "This is the first title",
-        "This is the second!",
-        "Third"
+    @State var items: [ItemModel] = [
+        ItemModel(title: "This is the first title", isCompleted: false),
+        ItemModel(title: "This is the second", isCompleted: true),
+        ItemModel(title: "Third", isCompleted: false)
     ]
     
     var body: some View {
-        List() {
-            ForEach(items, id: \.self) { item in
-                ListRowView(title: item)
-            }
+        List(items) { item in
+            ListRowView(item: item)
         }
         .listStyle(.plain)
         .navigationTitle("Todo List 📝")
@@ -27,7 +25,7 @@ struct ListView: View {
             ToolbarItem(placement: .topBarLeading) {
                 EditButton()
             }
-                
+            
             ToolbarItem(placement: .topBarTrailing) {
                 NavigationLink("Add", destination: AddView())
             }
